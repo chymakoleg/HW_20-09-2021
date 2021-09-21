6df48ca3aab66f4f8bfc42d6f26cc704211c3e84
chymakoleg <chymakoleg@gmail.com>
2021-09-21 20:57:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,117 @@
+
+//Tasck 1
+
+// const arrStrs1 = ['a', 'b', 'c', 'd'];
+// console.log(arrStrs1);
+// const arrStrs2 =[];
+
+// let limit = arrStrs1.length;
+
+// arrStrs2 [0] = arrStrs1 [3];
+// arrStrs2 [1] = arrStrs1 [0];
+// arrStrs2 [2] = arrStrs1 [3];
+// arrStrs2 [3] = arrStrs1 [0];
+
+// console.log(arrStrs2);
+
+
+
+// Tasck 2
+
+// const arrStrs1 = ['a', 'b', 'c', 'd'];
+// console.log(arrStrs1);
+// const arrStrs2 =[];
+
+// let limit = arrStrs1.length;
+
+// arrStrs2 [0] = arrStrs1 [3];
+// arrStrs2 [1] = arrStrs1 [2];
+// arrStrs2 [2] = arrStrs1 [1];
+// arrStrs2 [3] = arrStrs1 [0];
+
+// console.log(arrStrs2);
+
+
+
+// Tasck 3
+
+// const arrStrs1 = ['a', 'b', 'a', 'b'];
+// console.log(arrStrs1);
+// const arrStrs2 =[];
+
+// let limit = arrStrs1.length;
+
+// arrStrs2 [0] = arrStrs1 [1];
+// arrStrs2 [1] = arrStrs1 [0];
+// arrStrs2 [2] = arrStrs1 [1];
+// arrStrs2 [3] = arrStrs1 [0];
+
+// console.log(arrStrs2);
+
+
+
+// Tasck 4
+
+// const arrStrs1 = ['a', 'b', 'c', 'd'];
+// console.log(arrStrs1);
+// const arrStr2 = [];
+
+// arrStrs1[0] = 'a1';
+// arrStrs1[1] = 'b1';
+// arrStrs1[2] = 'c3';
+// arrStrs1[3] = 'd4';
+
+// let limit = arrStrs1.length;
+
+// arrStr2 [0] = arrStrs1 [0];
+// arrStr2 [1] = arrStrs1 [1];
+// arrStr2 [2] = arrStrs1 [2];
+// arrStr2 [3] = arrStrs1 [3];
+
+
+// console.log(arrStr2);
+
+
+
+// Tasck 5
+
+// const arrStrs1 = ['a', 'b', 'c', 'd'];
+// console.log(arrStrs1);
+// const arrStr2 = [];
+
+// arrStrs1[0] = 'ab';
+// arrStrs1[1] = 'bc';
+// arrStrs1[2] = 'cd';
+// arrStrs1[3] = 'da';
+
+// let limit = arrStrs1.length;
+
+// arrStr2 [0] = arrStrs1 [0];
+// arrStr2 [1] = arrStrs1 [1];
+// arrStr2 [2] = arrStrs1 [2];
+// arrStr2 [3] = arrStrs1 [3];
+
+
+// console.log(arrStr2);
+
+
+
+// Tasck 6
+
+// const arrStrs1 = ['a', 'b', 'c', 'd'];
+// console.log(arrStrs1);
+// const arrStr2 = [];
+
+// arrStrs1[0] = '25';
+// arrStrs1[1] = '-16';
+// arrStrs1[2] = '9';
+// arrStrs1[3] = '-4';
+
+// let limit = arrStrs1.length;
+
+// arrStr2 [0] = arrStrs1 [0];
+// arrStr2 [1] = arrStrs1 [1];
+// arrStr2 [2] = arrStrs1 [2];
+// arrStr2 [3] = arrStrs1 [3];
+
+// console.log(arrStr2);
